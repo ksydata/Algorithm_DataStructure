db77279485081eb5decee1d309daa54bef8663ab
Sooyeon Kang <112922030+ksydata@users.noreply.github.com>
2022-11-16 00:10:48 +0900

    Update algorithm_array.py

--- algorithm_array.py
+++ algorithm_array.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         # [브루스 포스로 풀이]
+        # [합이 "0"인 3수 리턴]
         
+        # [SY first version]
+        nums = nums.sort() 
+                # [일단 정렬]
+
+        for n in nums:
+            n += 1
+            if nums[n:n+2].sum() == 0:
+                return nums[n] nums[n+1] nums[n+2]
+            else: 
+                # [Error] Invalid Syntax
+                
 
