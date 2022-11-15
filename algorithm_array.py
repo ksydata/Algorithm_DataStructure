adf4c2377b7b927c128548170f2daaa75155ef91
Sooyeon Kang <112922030+ksydata@users.noreply.github.com>
2022-11-16 00:29:33 +0900

    Update algorithm_array.py

--- algorithm_array.py
+++ algorithm_array.py
@@ -39,4 +39,17 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i = 0
+        j = 0
+
+        if i in range(len(nums)):
+            if j in range(i + 1, len(nums)):
+            # [Runtime Error]
+            
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
+
 
