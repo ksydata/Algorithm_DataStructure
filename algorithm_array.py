c7e19221e1f8a1c925c55b33f5b9c0577c390f28
Sooyeon Kang <112922030+ksydata@users.noreply.github.com>
2022-11-16 00:26:17 +0900

    Update algorithm_array.py

--- algorithm_array.py
+++ algorithm_array.py
@@ -20,5 +20,23 @@ class Solution:
                 return nums[n] nums[n+1] nums[n+2]
             else: 
                 # [Error] Invalid Syntax
+    
+   
+# 7.1.(1) 두 수의 합
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # [덧셈하여 타겟을 만들 수 있는 배열의 두 숫자 인덱스 리턴]
+        # [조건: 두 수의 합 = 9]
+
+        nums = nums.sort()
+        i, j = 0, 0
+        if i in range(0, len(nums) - 1):
+            if j in range(1, len(nums) - 1):
+                # [시간복잡도 i, j O(n**2)]    
+                # [코드 에러] range() 문법, i & j 초기화, j의 range()
                 
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
 
